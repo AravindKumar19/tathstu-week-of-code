064b7d409891599068f4fb2b412befc5a4a4ba95
aravind222148 <65997388+aravind222148@users.noreply.github.com>
2020-05-28 07:23:57 +0530

    Create program3.py

---
+++ day1/program3.py
@@ -0,0 +1,7 @@
+number1=int(input("Enter The First Number"))
+number2=int(input("Enter The Second Number"))
+number1=number1+number2
+number2=number1-number2
+number1=number1-number2
+print("After Swap Number1 Is:",number1)
+print("After Swap Number2 Is ",number2)
