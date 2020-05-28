5e824ae12920b62aece8e0720ef92e835779dffe
aravind222148 <65997388+aravind222148@users.noreply.github.com>
2020-05-28 08:08:02 +0530

    Create program4.py

---
+++ day1/program4.py
@@ -0,0 +1,7 @@
+Costprice=float(input("Enter The costprice of objects"))
+Sellingprice=float(input("Enter The Selling price of objects"))
+profit=Sellingprice-Costprice
+print("profit is ",profit)
+print("New profit, increaing profit by 5%")
+newsellingprice=profit+profit*0.05+Costprice
+print("newselling price",newsellingprice)
